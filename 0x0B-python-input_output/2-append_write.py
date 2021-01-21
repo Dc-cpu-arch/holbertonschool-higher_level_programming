3d6ad18a39872dadd808b9f78ca7cfd8bf9f7b0a
Dc-cpu-arch <d.garzong77@gmail.com>
2021-01-21 02:41:56 +0000

    filename correction

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -7,4 +7,4 @@ Appends to text file and returns number of chars added
 def append_write(filename="", text=""):
     """Appends to text file and Returns number of chars added"""
     with open(filename, mode="a", encoding="utf-8") as file:
-        return(f.write(text))
+        return(file.write(text))
