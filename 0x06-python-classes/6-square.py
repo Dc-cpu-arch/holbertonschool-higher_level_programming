b321057c06a6b64cf04c1717029c140b8e10ba8a
Dc-cpu-arch <d.garzong77@gmail.com>
2021-02-10 22:24:22 +0000

    Equals Fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -21,7 +21,7 @@ class Square:
         """ Sets the size to the Instance """
         if type(value) != int:
             raise TypeError('size must be an integer')
-        if value <= 0:
+        if value < 0:
             raise ValueError('size must be >= 0')
         self.__size = value
 
