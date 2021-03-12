9ed284c94f288cfea2ce9e2f52eb7c24cc03a2a6
Dc-cpu-arch <d.garzong77@gmail.com>
2021-03-12 20:12:20 +0000

    PEP8

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,40 +1,35 @@
 #!/usr/bin/python3
-""" Definition of a Square Object """
+"""Square Class"""
 
 
 class Square:
-    """ Square attributes an methods """
-
+    """Empty Square class"""
     def __init__(self, size=0):
-        """ Initializes the object for every instance """
-
-        self.size = size
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
 
     @property
     def size(self):
-        """   Gets information about an instance   """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """ Sets value as instance size """
-
         if type(value) != int:
             raise TypeError('size must be an integer')
         if value < 0:
             raise ValueError('size must be >= 0')
         self.__size = value
 
-    def area(self):
-        """ Returns the area of the square """
-        return self.__size * self.__size
-
     def my_print(self):
-        """ Prints the square in stdout with '#' """
-        if self.__size == 0:
+        if self.size == 0:
             print()
-        else:
-            for i in range(self.__size):
-                for j in range(self.__size):
-                    print('#', end="")
-                print()
+        for i in range(self.size):
+            for j in range(self.size):
+                print('#', end='')
+            print()
+
+    def area(self):
+        return self.__size ** 2
