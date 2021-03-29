635608fddbe8a04230e2731c09373d7e364a5987
Dc-cpu-arch <d.garzong77@gmail.com>
2021-03-29 02:08:37 +0000

    Variable Fix

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -4,14 +4,16 @@ lists all State objects from the database hbtn_0e_6_usa
 '''
 
 from sys import argv
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+from model_state import Base, State
 
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(argv[1], argv[2], argv[3]))
+	Base.metadata.crate_all(engine)
+
     InstanceSession = sessionmaker(bind=engine)
     session = InstanceSession()
 
