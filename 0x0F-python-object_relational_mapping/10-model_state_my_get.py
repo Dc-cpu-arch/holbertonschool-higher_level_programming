c557a276d861c4d63ae1f8789f54751fcb6d615a
Dc-cpu-arch <d.garzong77@gmail.com>
2021-03-29 02:22:56 +0000

    Style Fix

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -9,7 +9,6 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 
-
 if __name__ == "__main__":
     engine = create_engine(
                 'mysql+mysqldb://{}:{}@localhost:3306/{}'
