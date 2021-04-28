1b6754b6e0ec8c3d85289b3e0cbe4f92f4821b39
Dc-cpu-arch <d.garzong77@gmail.com>
2021-04-28 00:16:48 -0500

    Syntax Fix

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- Creates a user if not exists
-CREATE USER IF NOT EXISTS 'user_0d_1' @'localhost' IDENTIFIED Bd_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1' @'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1' @'localhost' IDENTIFIED BY 'user_0d_1_pwd';
