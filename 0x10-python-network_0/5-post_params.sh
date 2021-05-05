d73f3c97ed2dba662521b194904d1ba878e85a39
Dc-cpu-arch <d.garzong77@gmail.com>
2021-05-05 16:22:29 -0500

    send a POST request

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes a URL, sends a POST request and displays the body of the response
-curl -sd "email=hr@holbertonschool.com&subject=I%20will%20allways%20be%20here%20for%20PLD" "$1"
+curl -s -X POST -d "email=hr@holbertonschool.com&subject=I will allways be here for PLD" "$1"
