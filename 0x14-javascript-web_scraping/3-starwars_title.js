23d0304893db7aeb9c47bd72ff1c2ab7912175fa
Dc-cpu-arch <d.garzong77@gmail.com>
2021-05-04 23:33:35 -0500

    Style Fix

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -9,3 +9,13 @@ request(URL + process.argv[2], (err, res, body) => {
   console.log(JSON.parse(body).title);
 })
 
+#!/usr/bin/node
+const request = require('request');
+
+const URL = 'https://swapi-api.hbtn.io/api/films/';
+request(URL + process.argv[2], (err, res, body) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(JSON.parse(body).title);
+});
