5fa377c6cd410d40b6bec72d57ba58d1e82547bd
Dc-cpu-arch <d.garzong77@gmail.com>
2021-05-04 23:30:31 -0500

    Logic Fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,13 +4,16 @@ let FilmIs = 0;
 
 request(process.argv[2], function (err, response, body) {
   if (err == null) {
-	  const res = JSON.parse(body);
-	  const results = res.results;
-	  for (let i = 0; i < results.length; i++) {
-		  if (characters[j].search('18') > 0) {
+    const res = JSON.parse(body);
+    const results = res.results;
+    for (let i = 0; i < results.length; i++) {
+      const characters = results[i].characters;
+      for (let j = 0; j < characters.length; j++) {
+        if (characters[j].search('18') > 0) {
 			  FilmIs++;
-		  }
-	  }
+        }
+      }
+    }
   }
   console.log(FilmIs);
 });
